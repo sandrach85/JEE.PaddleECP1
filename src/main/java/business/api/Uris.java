3db5b4540eef467b41b1a3a85fe57b77b86d84f8
Sandra <Sandra@192.168.0.164>
2016-03-18 14:32:29 +0100

    Añadidas uris para trainingResource

--- src/main/java/business/api/Uris.java
+++ src/main/java/business/api/Uris.java
@@ -1,23 +1,35 @@
 package business.api;
 
 public class Uris {
-    
+
     public static final String SERVLET_MAP = "/api/v0";
 
     public static final String ID = "/{id}";
-    
+
     public static final String ADMINS = "/admins";
-    
+
     public static final String USERS = "/users";
 
     public static final String TOKENS = "/tokens";
-    
+
     public static final String COURTS = "/courts";
-    
+
     public static final String ACTIVE = "/active";
-    
+
     public static final String RESERVES = "/reserves";
-    
+
     public static final String AVAILABILITY = "/availability";
+
+    public static final String TRAINING = "/training";
+
+    public static final String CREATE_TRAINING = "/createTraining";
+
+    public static final String DELETE_TRAINING = "/deleteTraining";
+
+    public static final String DELETE_TRAINING_PLAYER = "/deleteTrainingPlayer";
+
+    public static final String SHOW_TRAININGS = "/showTrainings";
+
+    public static final String REGISTER_TRAINING = "/registerTraining";
 
 }
