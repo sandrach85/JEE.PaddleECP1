7351cfc30f8b864cb02bbdd86194eb8d40dc3670
Sandra <Sandra@192.168.0.164>
2016-03-18 20:43:19 +0100

    Añadida y terminada parte web de pistas

---
+++ src/main/java/business/web/CourtPresenter.java
@@ -0,0 +1,52 @@
+package business.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import business.controllers.CourtController;
+import business.wrapper.CourtState;
+
+@Controller
+public class CourtPresenter {
+	
+	@Autowired
+	private CourtController courtController;
+	
+	
+	public CourtPresenter(){
+		
+	}
+	
+	@RequestMapping("/home")
+    public String home(Model model) {
+        //model.addAttribute("themes", THEMES);
+        //La vista resultante no lleva extensión (.jsp) configurado en WebConfig.java
+        return "/home";
+    }
+
+	@RequestMapping("/list-courts")
+	public String listCourts(Model model){
+        model.addAttribute("courtList", courtController.showCourts());
+        return "showCourtList";
+	}
+	
+	@RequestMapping(value ="/create-court", method = RequestMethod.GET)
+	public String createCourt(Model model){
+        model.addAttribute("court", new CourtState(courtController.showCourts().size()+1, true));
+        return "createCourt";
+	}
+	
+	@RequestMapping(value = "/create-court", method = RequestMethod.POST)
+	public String createUserSubmit(@ModelAttribute(value="court") CourtState court, Model model){
+		courtController.createCourt(court.getCourtId());
+		//int numCourts = courtController.showCourts().size();
+        //model.addAttribute("court", new CourtState(numCourts+1, true));
+		this.createCourt(model);
+		return "/createCourtSuccess";
+		
+	}
+}
